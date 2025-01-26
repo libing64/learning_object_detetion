95131d8ff3ef63af91e85667280db6faa98ba071
bingli <bingli@deeproute.ai>
2025-01-26 21:59:31 +0800

    format code with vscode

--- yolo_fine_tune_test.py
+++ yolo_fine_tune_test.py
@@ -10,23 +10,24 @@ model = YOLO("runs/detect/train2/weights/best.pt")  # pretrained YOLOv8n model
 # image_dir = "/home/libing/dataset/AquariumDataset/AquariumCombined.v2i.voc/train"
 image_dir = "/home/libing/dataset/Air_Plane_Detection.v3i.yolov8/train/images"
 
-image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
+image_files = [f for f in os.listdir(
+    image_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
 os.makedirs("result", exist_ok=True)
 
 for image_file in image_files:
     image_path = os.path.join(image_dir, image_file)
-    
+
     results = model([image_path])
-    
+
     for result in results:
-        boxes = result.boxes  
-        masks = result.masks  
-        keypoints = result.keypoints 
-        probs = result.probs 
-        obb = result.obb  
-        
+        boxes = result.boxes
+        masks = result.masks
+        keypoints = result.keypoints
+        probs = result.probs
+        obb = result.obb
+
         result.show()
-        
+
         result_filename = f"result/{image_file}"
         result.save(filename=result_filename)
